58e3ebcba9fbd350fa92a5a58fd880f290124c81
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-15 11:43:29 +0330

    Add support access audit action enum

---
+++ app/Domain/Server/Enums/SupportAccessAction.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Server\Enums;
+
+enum SupportAccessAction: string
+{
+    case SshConnectionTest = 'ssh_connection_test';
+    case CredentialRevealed = 'credential_revealed';
+}
