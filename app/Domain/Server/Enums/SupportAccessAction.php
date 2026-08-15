18126e4c55c6f6620e27a0d98857683ae7e4cc53
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-15 13:12:53 +0330

    Audit support Passkey confirmations

--- app/Domain/Server/Enums/SupportAccessAction.php
+++ app/Domain/Server/Enums/SupportAccessAction.php
@@ -7,5 +7,6 @@ namespace App\Domain\Server\Enums;
 enum SupportAccessAction: string
 {
     case SshConnectionTest = 'ssh_connection_test';
+    case PasskeyConfirmed = 'passkey_confirmed';
     case CredentialRevealed = 'credential_revealed';
 }
