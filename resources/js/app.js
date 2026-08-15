ad0fbe5e14f03db6345487d922aa58c56c00bbf0
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-15 12:26:25 +0330

    Bundle Passkeys and EasyMDE locally

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,14 @@
+import 'easymde/dist/easymde.min.css';
+import EasyMDE from 'easymde';
+
 import '../css/admin-markdown.css';
+import './passkeys.js';
 
 import {
     registerLivewireRequestErrorHandler,
 } from './livewire/request-error-handler.js';
+
+window.EasyMDE = EasyMDE;
 
 document.addEventListener(
     'livewire:init',
