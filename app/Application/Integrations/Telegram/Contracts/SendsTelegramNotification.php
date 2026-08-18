5d0ac06b53ffaf183ea1d7bb1955fae5ab89a710
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-18 15:45:33 +0330

    Expose Telegram notification preference topic

--- app/Application/Integrations/Telegram/Contracts/SendsTelegramNotification.php
+++ app/Application/Integrations/Telegram/Contracts/SendsTelegramNotification.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Application\Integrations\Telegram\Contracts;
 
 use App\Application\Integrations\Telegram\TelegramMessage;
+use App\Application\Notifications\NotificationTopic;
 
 interface SendsTelegramNotification
 {
+    public function telegramTopic(): NotificationTopic;
+
     public function toTelegram(object $notifiable): TelegramMessage;
 }
