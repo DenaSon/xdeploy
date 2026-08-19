9df2448cc7453549b40ab28a845401cfda5ad8ae
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-20 00:42:01 +0330

    Trust forwarded HTTPS proxy headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withCommands()
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO,
+        );
+
         $middleware->alias([
             'admin' => EnsureUserIsAdmin::class,
             'admin.passkey' => EnsureAdminPasskeyVerified::class,
