deac7c28ba578811c0b7ed01a447542190dcf9f7
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-22 21:07:06 +0330

    Add system settings snapshot DTO

---
+++ app/Application/Settings/SystemSettingsSnapshot.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Settings;
+
+final readonly class SystemSettingsSnapshot
+{
+    public function __construct(
+        public string $siteName,
+        public string $tagline,
+        public string $seoDefaultTitle,
+        public string $seoDefaultDescription,
+        public ?string $seoDefaultOgImage,
+        public bool $seoIndexSite,
+    ) {}
+}
