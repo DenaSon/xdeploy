8c4cc2b6b86f30353d0a296b3e35b1a63be56e8f
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-23 18:34:37 +0330

    Remove AmneziaWG provider expectation

--- tests/Feature/Providers/ApplicationServiceProviderTest.php
+++ tests/Feature/Providers/ApplicationServiceProviderTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Providers;
 
-use App\Domain\Application\AmneziaWg\AmneziaWgApplication;
 use App\Domain\Application\Contracts\ApplicationRegistryInterface;
 use App\Domain\Application\Shared\Enums\ApplicationType;
 use App\Domain\Application\WordPress\WordPressApplication;
@@ -12,20 +11,6 @@ use Tests\TestCase;
 
 final class ApplicationServiceProviderTest extends TestCase
 {
-    public function test_application_registry_resolves_amneziawg(): void
-    {
-        $registry = $this->app->make(
-            ApplicationRegistryInterface::class,
-        );
-
-        $this->assertInstanceOf(
-            AmneziaWgApplication::class,
-            $registry->find(
-                ApplicationType::AmneziaWg,
-            ),
-        );
-    }
-
     public function test_application_registry_resolves_wordpress(): void
     {
         $registry = $this->app->make(
