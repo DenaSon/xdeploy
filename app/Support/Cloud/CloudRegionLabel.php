db84e4dee26434dae3527ed45add8552d7666bd0
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-08-28 15:55:29 +0330

    Add ParsPack region labels

--- app/Support/Cloud/CloudRegionLabel.php
+++ app/Support/Cloud/CloudRegionLabel.php
@@ -13,6 +13,18 @@ final class CloudRegionLabel
         'ir-tbz-sh1' => 'غرب ایران · آذر',
         'ir-southwest1-a' => 'جنوب‌غرب · کارون',
         'eu-west1-a' => 'اروپا · گوته',
+
+        'frankfurt' => 'آلمان · فرانکفورت',
+        'amsterdam' => 'هلند · آمستردام',
+        'tehran2' => 'ایران · تهران',
+        'tehran3' => 'ایران · تهران',
+        'tehran11' => 'ایران · تهران',
+        'tehran16' => 'ایران · تهران',
+        'london1' => 'بریتانیا · لندن',
+        'istanbul' => 'ترکیه · استانبول',
+        'paris' => 'فرانسه · پاریس',
+        'toronto2' => 'کانادا · تورنتو',
+        'stockholm' => 'سوئد · استکهلم',
     ];
 
     public static function for(
